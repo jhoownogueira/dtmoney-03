03d5faa2ee136b7bc4136aa34a2b9eb15dd20238
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-11-21 09:58:52 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyled } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyled />
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
