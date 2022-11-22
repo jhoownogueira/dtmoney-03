15b51b71930da1cca13cc74dc1ef117c6fe593cf
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-11-22 14:17:47 -0300

    Dialog Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import {
   HeaderContainer,
   HeaderContent,
@@ -10,7 +11,18 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} />
-        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova transação</NewTransactionsButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
