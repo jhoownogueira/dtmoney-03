24a857df658f3a67b03c13a631319d8bd51f75d1
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-11-25 08:38:01 -0300

    Criando contexto de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyled } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -7,7 +8,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyled />
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
